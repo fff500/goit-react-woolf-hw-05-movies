0f4da8fe7dc327c3affab469380bdcbfeb46ca18
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-03-03 19:54:50 +0200

    Application was initialised

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { lazy } from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const Movies = lazy(() => import('./pages/Movies/Movies'));
+const Movie = lazy(() => import('./pages/Movie/Movie'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" element={<Movie />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
+  );
+};

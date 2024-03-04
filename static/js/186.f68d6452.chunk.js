c419533d15575b584c8e540323acec28f7102e31
fff500 <pavlo.tereshko@gmail.com>
2024-03-04 10:50:03 +0000

    Deploying to gh-pages from @ fff500/goit-react-woolf-hw-05-movies@bb7a76d2900c497068bb32453d7f8617a6bd2a5e 🚀

---
+++ static/js/186.f68d6452.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[186],{186:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=186.f68d6452.chunk.js.map

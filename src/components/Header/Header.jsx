05b725de7bc1b13f8d2490416c5c56fe7c183727
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-03-04 18:33:34 +0200

    Layout and Header components were added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,28 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 import styles from './Header.module.css';
 
-export const Header = () => {
-  return (
-    <header className={styles.header}>
-      <nav>
-        <ul className={styles.navList}>
-          <li>
-            <NavLink to={'/home'} className={styles.navLink}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={'./movies'} className={styles.navLink}>Movies</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+export const Header = () => (
+  <header className={styles.header}>
+    <nav>
+      <ul className={styles.navList}>
+        <li>
+          <NavLink
+            to={'/home'}
+            className={({ isActive }) => `${styles.navLink} ${isActive && styles.active}`}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={'./movies'}
+            className={({ isActive }) => `${styles.navLink} ${isActive && styles.active}`}
+          >
+            Movies
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,18 @@
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
 
-import { Header } from "components/Header/Header";
+import { Header } from 'components/Header/Header';
 
 import style from './Layout.module.css';
 
-export const Layout = () => {
-  return (
-    <>
-      <Header />
+export const Layout = () => (
+  <>
+    <Header />
 
-      <main className={style.main}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Outlet />
-        </Suspense>
-      </main>
-    </>
-  );
-};
+    <main className={style.main}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Outlet />
+      </Suspense>
+    </main>
+  </>
+);

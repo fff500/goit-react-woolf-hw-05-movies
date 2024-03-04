a2c2e8e5584a1f3dec6adeaf6308d10a6075fc7e
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-03-04 19:22:27 +0200

    Movies page was added

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,40 +1,56 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
+import { MovieLink } from 'components/MovieLink/MovieLink';
 import { getMoviesByKeyWord } from 'api/themoviedb-api';
+import { DEFAULT_ERROR_MESSAGE } from 'constants/constants';
+
+import style from './Movies.module.css';
 
 const Movies = () => {
   const [setsearchResults, setSetsearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [, setSearchParams] = useSearchParams();
 
   const handleSearch = async (event) => {
     event.preventDefault();
 
-    const query = event.target.elements[0].value.trim().toLowerCase();
-
-    const { data: { results } } = await getMoviesByKeyWord(query);
-
-    setSetsearchResults(results);
+    try {
+      setIsLoading(true);
+      const query = event.target.elements[0].value.trim().toLowerCase();
+      const { data: { results } } = await getMoviesByKeyWord(query);
+      setSearchParams({ query });
+      setSetsearchResults(results);
+      event.target.elements[0].value = '';
+    } catch (error) {
+      alert(error.message || DEFAULT_ERROR_MESSAGE);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   return (
     <div>
-      <form onSubmit={handleSearch}>
-        <input type="text" name="query" id="query" />
+      <form onSubmit={handleSearch} className={style.form}>
+        <input type="text" name="query" required />
         <button type="submit">Search</button>
       </form>
-      {!!setsearchResults.length && <ul>
-        {setsearchResults.map(({ id, original_name, title }) => {
-          return (
-            <li key={id}>
-              <Link to={`/movies/${id}`}>
-                {title || original_name}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>}
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && !!setsearchResults.length && (
+        <ul>
+          {setsearchResults.map(({ id, title, name, original_name }) => (
+            <MovieLink
+              key={id}
+              to={`/movies/${id}`}
+              text={title || name || original_name}
+            />
+          ))}
+        </ul>
+      )}
+      {!isLoading && !setsearchResults.length && 'Sorry! No movies were found'}
     </div>
   )
-}
+};
 
-export default Movies
+export default Movies;

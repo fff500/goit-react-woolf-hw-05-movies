46130e5032eacb05f978001dc336c6fcc58fa3c1
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-03-04 23:19:39 +0200

    MovieDetails page was added

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,74 +1,90 @@
 import React, { useEffect, useState, Suspense } from 'react';
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from 'react-router-dom';
 
-import { getMovie } from "api/themoviedb-api";
-import { BASE_API_URL } from 'constants/constants';
+import { getMovie } from 'api/themoviedb-api';
+import { DEFAULT_ERROR_MESSAGE } from 'constants/constants';
+
+import style from './MovieDetails.module.css';
 
 const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   const { movieId } = useParams();
 
   useEffect(() => {
-    const fetchMovie = async (movieId) => {
+    const handleLoadMovie = async (movieId) => {
       try {
-        const { data: { original_title, title, popularity, overview, genres, poster_path, release_date } } = await getMovie(movieId);
-
-        setMovieDetails({
-          title: title || original_title,
+        setIsLoading(true);
+        const { data: {
+          original_title,
+          title,
+          name,
           popularity,
           overview,
           genres,
-          posterSrc: `${BASE_API_URL}${poster_path}`,
+          poster_path,
+          release_date
+        } } = await getMovie(movieId);
+
+        setMovieDetails({
+          title: title || name || original_title,
+          popularity,
+          overview,
+          genres,
+          posterSrc: poster_path,
           releasYear: release_date.substring(0, 4),
         });
       } catch (error) {
-        alert(error.message);
+        alert(error.message || DEFAULT_ERROR_MESSAGE);
+      } finally {
+        setIsLoading(false);
       }
     }
 
-    fetchMovie(movieId);
+    handleLoadMovie(movieId);
 
   }, [movieId])
 
-  console.log(movieDetails);
-
   return (
     <>
-      {movieDetails && (<div>
-        <button>Go back</button>
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && movieDetails && (
         <div>
-          <div>
-            <img src={movieDetails.posterSrc} alt={`${movieDetails.title} poster`} />
-          </div>
-          <div>
-            <h1>{`${movieDetails.title} (${movieDetails.releasYear})`}</h1>
-            <p>User score: {movieDetails.popularity}</p>
-            <div>
-              <h2>Overview</h2>
-              <p>{movieDetails.overview}</p>
+          <button>Go back</button>
+          <div className={style.detailsContainer}>
+            <div className={style.imageContainer}>
+              <img src={movieDetails.posterSrc} alt={`${movieDetails.title} poster`} />
             </div>
             <div>
-              <h3>Genres</h3>
-              <ul>
-                {movieDetails.genres.map(({ id, name }) => {
-                  return <li key={id}>{name}</li>
-                })}
-              </ul>
+              <h1>{`${movieDetails.title} (${movieDetails.releasYear})`}</h1>
+              <p>User score: {movieDetails.popularity}</p>
+              <div>
+                <h2>Overview</h2>
+                <p>{movieDetails.overview}</p>
+              </div>
+              <div>
+                <h3>Genres</h3>
+                <ul className={style.genresList}>
+                  {movieDetails.genres.map(({ id, name }) => <li key={id}>{name}</li>)}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>)}
-      <div>
+      )}
+      <div className={style.additionalLinksWrapper}>
         <p>Additional information</p>
         <ul className={'navList'}>
           <li>
-            <Link to={'cast'} className={'styles.navLink'}>Cast</Link>
+            <Link to={'cast'}>Cast</Link>
           </li>
           <li>
-            <Link to={'reviews'} className={'styles.navLink'}>Reviews</Link>
+            <Link to={'reviews'}>Reviews</Link>
           </li>
         </ul>
       </div>
+
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
@@ -76,4 +92,4 @@ const MovieDetails = () => {
   )
 }
 
-export default MovieDetails
+export default MovieDetails;
